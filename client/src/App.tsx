48f370d50ba72380d21e9e0d99c3f84bb171d7c0
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-04-18 12:59:39 -0900

    delete deck done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,58 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [decks, setDecks] = useState([])
-  const [title, setTitle] = useState("")
+  const [decks, setDecks] = useState<any>([]);
+  const [title, setTitle] = useState("");
 
   const handleCreateDeck = async (e: React.FormEvent) => {
-    e.preventDefault()
-    await fetch("http://localhost:5000/decks", {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/decks", {
       method: "POST",
       body: JSON.stringify({
         title,
       }),
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     });
-    setTitle("")
-  }
 
-  useEffect(()=> {
+    const newDeck: any = await response.json()
+
+    setDecks([...decks, newDeck])
+    setTitle("");
+  };
+
+  const handleDeleteDeck = async (deckId: string) => {
+    setDecks(decks.filter((deck: any) => deck._id !== deckId))
+   await fetch(`http://localhost:5000/decks/${deckId}`, {
+      method: "DELETE",
+    });
+
+  };
+
+  useEffect(() => {
     const fetchDecks = async () => {
       const response = await fetch("http://localhost:5000/decks");
-      const newDecks = await response.json()
-      setDecks(newDecks)
-    }
-    fetchDecks()
-  }, [])
+      const newDecks = await response.json();
+      setDecks(newDecks);
+    };
+    fetchDecks();
+  }, []);
 
   return (
     <main className="h-full flex flex-col items-center justify-center gap-4">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
         {decks.map((deck: any) => (
-          <div key={deck.id} className="p-2 border rounded-md">
-            {deck.title}
+          <div key={deck._id} className="p-2 border rounded-md">
+            <div className="flex items-center justify-between">
+              <h2>{deck.title}</h2>
+              <p
+                onClick={() => handleDeleteDeck(deck._id)}
+                className="py-1 px-2 flex items-center justify-center  hover:bg-slate-100 rounded-sm transition cursor-pointer text-muted-foreground"
+              >
+                x
+              </p>
+            </div>
           </div>
         ))}
       </div>
